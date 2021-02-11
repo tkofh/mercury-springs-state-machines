f957d31958fdec04204ba647ca250f047c2671c7
Tim Morris <timthewebguy@gmail.com>
2021-02-11 13:41:39 -0500

    update purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ const generateScale = function (parameters, options) {
 }
 
 module.exports = {
-  purge: ['./index.html', './coilix/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
